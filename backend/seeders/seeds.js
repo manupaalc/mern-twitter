9f4268af6ae351e3e07e5fed551b8f7147bbf8e6
Manuel P <m.perezasensio@gmail.com>
2023-08-28 08:57:02 -0700

    minor changes in seeds file

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -42,3 +42,32 @@ for (let i = 0; i < NUM_SEED_TWEETS; i++) {
         })
     )
 }
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB successfully');
+        insertSeeds();
+    })
+    .catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+    });
+
+// Reset and seed db
+const insertSeeds = () => {
+    console.log("Resetting db and seeding users and tweets...");
+
+    User.collection.drop()
+        .then(() => Tweet.collection.drop())
+        .then(() => User.insertMany(users))
+        .then(() => Tweet.insertMany(tweets))
+        .then(() => {
+            console.log("Done!");
+            mongoose.disconnect();
+        })
+        .catch(err => {
+            console.error(err.stack);
+            process.exit(1);
+        });
+}
